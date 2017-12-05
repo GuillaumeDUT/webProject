aafc935e17c8d74fdb3fdf72d0a12af2ccb0ac35
Auram20 <myriamanik97@gmail.com>
2017-12-05 11:16:04 +0100

    Musique qui passe quand réponse bonne

--- js/main.js
+++ js/main.js
@@ -97,12 +97,13 @@ function playEachMusic(){
 function onPlayerReady(event) {
 }
 
-cSkip.addEventListener('click',function(e){
+cSkip.addEventListener('click',musiquesuivante);
+
+function musiquesuivante(e){ 
+if (e)  
   e.preventDefault();
   playEachMusic();
-});
-
-
+}
 
 cSubmit.addEventListener('click',function(e){
   e.preventDefault();
@@ -115,12 +116,72 @@ cSubmit.addEventListener('click',function(e){
 
 
 function afficherTab(){
-  //console.log("nique");
   console.log(idArray);
   for(var i=0; i<idArray.length;i++){
     console.log(idArray[i]);
   }
 }
 
+
+
+
+var cInput = document.getElementById("inputreponse");
+cInput.addEventListener('keyup',function(e){
+    
+    if (e.keyCode == 13) {
+        if ( verifiereponse(idArray[0][1],cInput.value) >= 0.5) {
+        console.log('GG')
+            musiquesuivante();
+    }
+        else{
+            console.log('Perdu')
+        }
+    }
+  
+});
+
+
+
+function verifiereponse(s1, s2) {
+  var longer = s1;
+  var shorter = s2;
+  if (s1.length < s2.length) {
+    longer = s2;
+    shorter = s1;
+  }
+  var longerLength = longer.length;
+  if (longerLength == 0) {
+    return 1.0;
+  }
+  return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength);
+}
+
+
+function editDistance(s1, s2) {
+  s1 = s1.toLowerCase();
+  s2 = s2.toLowerCase();
+
+  var costs = new Array();
+  for (var i = 0; i <= s1.length; i++) {
+    var lastValue = i;
+    for (var j = 0; j <= s2.length; j++) {
+      if (i == 0)
+        costs[j] = j;
+      else {
+        if (j > 0) {
+          var newValue = costs[j - 1];
+          if (s1.charAt(i - 1) != s2.charAt(j - 1))
+            newValue = Math.min(Math.min(newValue, lastValue),
+              costs[j]) + 1;
+          costs[j - 1] = lastValue;
+          lastValue = newValue;
+        }
+      }
+    }
+    if (i > 0)
+      costs[s2.length] = lastValue;
+  }
+  return costs[s2.length];
+}
 //retrieveIdFromPlaylist();
 
